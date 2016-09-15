6616bff045430d9281fd30140e3ec65a5dd54ade
Aptdeploy <ops@aptrust.org>
2016-09-15 19:26:42 +0000

    Add explicit temp-dir to avoid out of space errors.

--- backup_aptrust.sh
+++ backup_aptrust.sh
@@ -3,4 +3,4 @@
 # Backup all fedora data, except the datastreamStore symlink, which
 # runs under a separate back and goes to a different destination drive.
 
-rsync -h -a ubuntu@repository.aptrust.org:/mnt/aptrust/ /mnt/aptrust/ --exclude "lost+found" --log-file=/home/ubuntu/aptrust_backup.log -e "ssh -p1984"
+rsync -h -a ubuntu@repository.aptrust.org:/mnt/aptrust/ /mnt/aptrust/ --exclude "lost+found" --log-file=/home/ubuntu/aptrust_backup.log -e "ssh -p1984" --temp-dir=/mnt/aptrust/temp
